52979df953704eab164ddb327609ded2f1d26231
nuuuwan <nuuuwan@gmail.com>
2023-08-25 06:59:12 +0530

    output format

--- small_files.py
+++ small_files.py
@@ -21,54 +21,50 @@ def is_valid_file_ext(file_path: str) -> bool:
 
 
 def get_n_lines(file_path: str) -> int:
-    if not is_valid_file_ext(file_path):
-        return None
-
     lines = File(file_path).read_lines()
-    n_lines = len(lines)
-    if n_lines <= 100:
-        return None
-
-    return n_lines
+    return len(lines)
 
 
 def get_long_file_info(dir_path):
     long_file_info_list = []
     for name_only in os.listdir(dir_path):
         dir_or_file_path = os.path.join(dir_path, name_only)
-        if is_valid_path(dir_or_file_path):
+        if not is_valid_path(dir_or_file_path):
             continue
 
         if os.path.isdir(dir_or_file_path):
-            long_file_info_list += get_long_file_info(dir_or_file_path)
+            long_file_info_list.extend(get_long_file_info(dir_or_file_path))
         else:
-            n_lines = get_n_lines(dir_or_file_path)
-            if n_lines:
-                long_file_info_list.append(
-                    dict(file_path=dir_or_file_path, n_lines=n_lines)
+            if not is_valid_file_ext(dir_or_file_path):
+                continue
+            long_file_info_list.append(
+                dict(
+                    file_path=dir_or_file_path,
+                    n_lines=get_n_lines(dir_or_file_path),
                 )
+            )
     return long_file_info_list
 
 
 def main():
     root_path = os.getcwd()
     long_file_info_list = get_long_file_info(root_path)
-    if not long_file_info_list:
-        return
-
+    len(long_file_info_list)
     print(' ')
-    print('-' * 32)
-    print('LONG FILES')
     print('-' * 32)
     sorted_long_file_info_list = sorted(
         long_file_info_list, key=lambda x: x['n_lines'], reverse=True
     )
     for long_file_info in sorted_long_file_info_list:
+        if long_file_info['n_lines'] <= 50:
+            continue
         print(
             long_file_info['n_lines'],
             long_file_info['file_path'].replace(root_path, '')[1:],
         )
-    print(' ')
+
+    total_n_lines = sum([x['n_lines'] for x in long_file_info_list])
+    print(f'{total_n_lines:,}', 'TOTAL')
     print('-' * 32)
     print(' ')
 
