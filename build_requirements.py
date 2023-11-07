685d3f527d90d3c9f5317ee28ff9da8418e056cb
nuuuwan <nuuuwan@gmail.com>
2023-11-07 08:59:09 +0530

    build_requirements

---
+++ build_requirements.py
@@ -0,0 +1,26 @@
+import os
+from unittest import mock
+
+import setuptools
+from utils import File, Log
+
+log = Log('build requirements')
+
+
+def main():
+    if not os.path.exists('setup.py'):
+        return
+    with mock.patch.object(setuptools, 'setup') as mock_setup:
+        pass
+
+        _, kwargs = mock_setup.call_args
+        install_requires = kwargs.get('install_requires', [])
+        file_path = 'requirements.txt'
+        File(file_path).write_lines(
+            ['# Autogenerate by build_requirements'] + install_requires
+        )
+        log.info(f'Wrote {len(install_requires)} dependencies to {file_path}')
+
+
+if __name__ == '__main__':
+    main()
